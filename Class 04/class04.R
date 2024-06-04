801f8099c389d9b623b253b684707c3e55221ac8
Cynthia Perez <c7perez@ucsd.edu>
2024-06-04 10:46:48 -0700

    Add gfm format to class website

---
+++ Class 04/class04.R
@@ -0,0 +1,10 @@
+#' ---
+#' title: "Class 4: R basics"
+#' author: "Cynthia Perez (PID:A16393492)"
+#' date: "April 11th, 2024"
+#' ---
+
+#This is just a text file
+x <- 1:50
+plot(x, sin(x))
+plot(x, sin(x), typ="l", col="red", lwd=3)
